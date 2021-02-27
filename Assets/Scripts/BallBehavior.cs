28b7526f9e09f1c0f9e32e07d1f72855778e1fba
rem <61942221+remxiia@users.noreply.github.com>
2021-02-26 19:54:57 -0500

    Initial commit

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    
+    SpriteRenderer myRenderer; //specifiying what the hypothetical box in the program can hold (spriterenders)
+    //variable myRenderer to type SpriteRenderer
+    public Color floorColor; //box that only holds colors, and this specific one is just for the floor's color
+    public color gateColor;
+    RigidBody2D myBody;
+    public float power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //19: setting the var
+        myRenderer = gameObject.GetComponent<SpriteRenderer>() //gameObj = the object being dealt with atm (the ball); getComp = refs components on the ball, and we want to change the spriteRenderer
+        //in a game that has physics: you don't want to reference the object's transform the way we did in the previous tutorial
+        //we need to move their body, not just their visual on the game screen
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){ //getkeydown: returns true once, not as if you're holding it down
+            //myBody.AddForce(Vector3.right); //adding a teeny bit of force and rolling the object
+            //Vector3.right is effectively writing it as 1, 0, 0; x, y, z | the same as: 
+            myBody.AddForce(new Vector3(1, 0, 0) * power); //* power adds much more force, equal to [power] under [ball behavior] in [inspector]
+
+            //in order to make the ball go up-right: We can add vectors together:
+            myBody.AddForce((Vector3.right * Vector3.up) * power); //in the smaller parenthesis will be carried out first
+            //OR w/coordinates you make yourself:
+            //myBody.AddForce((new Vector3(1, 0, 0) + new Vector3(0, 1, 0)) * power); //or doing (1, 1, 0) would get the same result
+        
+        }  
+    }
+
+    //this code won't do anything bc there's nothing in the project, but it's here for future reference purposes
+    //there are functions that will be called when a certain thing occurs, such as:
+    void onCollisionEnter2D(Collision2D other){ //in paranthesis: parameter that says when the object hits something else, we get the info/data from that collision 
+    //(how much force their was, what it collided with, etc.)
+       if(other.gameObject.name == "Floor"){ //if the other game object is the floor, then...
+            myRenderer.color = floorColor; //lowercase bc it's a property // can do this bc line 19
+            Debug.Log("Hit something");//...display this message in the console.
+       }
+    }
+        
+    void OnTriggerEnter2D(Collider2D other){ //trigger's don't have anything to do with physics; don't need the extra data we get from Collision2D...
+    //...we just need to know info about the other object
+        if(other.gameObject.name == "Gate){
+            myRenderer.color = gateColor;
+        }
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTransform; //follows the player
+    public BoxCollider2D worldBounds; //sets boundaries for the world so the camera can't leave it
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax; //will hold the position of cam
+
+    float camX;
+    float camY; //position of our camera
+
+    float camRatio; //y width of camera
+    float camSize; //x length of camera
+
+    Camera mainCam; //ref to camera component
+
+    Vector3 smoothPos; //math for the staggered effect of our cam following the ball
+
+    public float smoothRate; //speed, how quickly the cam will watch up to the player when it moves
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //adding smoothing: freeform movement for the camera so it's not locked onto the player
+        xMin = worldBounds.bounds.min.x; //xMin will hold worldbounds, bounds (box collider components), minimum side of field, x
+        xMax = worldBounds.bounds.max.x;
+        yMin = worldBounds.bounds.min.y;
+        yMax = worldBounds.bounds.min.y;
+
+        mainCam = gameObject.GetComponent<Camera>();
+
+        camSize = mainCam.orthographicSize;
+        camRatio = (xMax + camSize) / 8.0f; //finding the y and x of the camera, then moving it in the middle
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //you DONT want to do what's in FixedUpdate in Update bc it'll lag the camera too much
+    }
+
+    void FixedUpdate() //there is a fixed rate at which FixedUpdate is running. Anything physics related = in FixedUpdate
+    {
+        camY = Mathf.Clamp(followTransform.position.y, yMin + camSize, yMax - camSize);
+        camX = Mathf.Clamp(followTransform.position.x, xMin + camRatio, xMax - camRatio);
+
+        smoothPos = Vector3.Lerp(gameObject.transform.position, new Vector3(camX, camY, gameObject))
+        //first parameter = current pos, second par = where we want the camera to move, third par = the rate that we want to move at
+
+        gameObject.transform.position = smoothPOs; //set my position to what all that math above is doing
+
+        //!!!Create a new object for world bounds!!! - give it a box collider component 
+    }
+}
